08ae1b9ee3f16a281fd697470ebf7449abeb1860
Allen Leis <allen.leis@threespot.com>
2013-04-22 22:41:05 -0400

    removes bundle from repo

--- rghost_barcodes/bundle/ruby/1.9.1/gems/rghost_barcode-0.9/lib/rghost_barcode.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH << File.join(File.dirname(__FILE__),'rghost_barcode')
-require 'rghost_barcode/rghost_barcode_base'
-require 'rghost_barcode/rghost_barcode_types'
-require 'rghost_barcode/rghost_barcode_version'
-require 'rghost_barcode/rghost_barcode_adapter'
-
-RGhost::Config::GS[:extensions] << File.join(File.dirname(__FILE__),'rghost_barcode','ps')
-RGhost::Config::GS[:preload] << "barcode_base"
-
--- rghost_barcodes/bundle/ruby/1.9.1/gems/rghost_barcode-0.9/lib/rghost_barcode/rghost_barcode_version.rb
+++
@@ -1,8 +0,0 @@
-module RGhost::Barcode::VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 9
-    TINY  = 0
-    DATE=1301238712
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  
-end
